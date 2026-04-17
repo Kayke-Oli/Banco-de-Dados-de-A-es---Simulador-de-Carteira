942378db1d26823c2250c39405e26aa6605070ef
Kayke <kaykerodr5@gmail.com>
2026-04-17 17:36:22 -0300

    Primeira implementação da classe Data

--- data.cpp
+++ data.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
 #include "data.h"
+#include <string>
+
+Data::Data(int data){
+    this->_data = data;
+}
+
+std::string Data::converte(){
+    std::string ds = std::to_string(_data);
+    return ds;
+}
+
+bool Data::compara(const int& a, const int& b){
+    if(a > b)
+        return true;
+    return false;
+}
--- data.h
+++ data.h
@@ -1,4 +1,13 @@
 #ifndef DATA_H
 #define DATA_H
+#include <string>
 
+class Data{
+private:
+    int _data;
+public:
+    Data(int data);
+    std::string converte();
+    bool compara(const int& a,const int& b);
+};
 #endif
