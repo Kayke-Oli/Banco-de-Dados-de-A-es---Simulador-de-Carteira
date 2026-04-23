324cf3d3ca9d1165ece8fc59a8dbfa7398292be3
Kayke <kaykerodr5@gmail.com>
2026-04-23 14:18:19 -0300

    Alterações na classe data

--- data.cpp
+++ data.cpp
@@ -1,18 +1,53 @@
 #include <iostream>
 #include "data.h"
 #include <string>
+#include <iomanip>
+#include <cassert>
 
-Data::Data(int data){
-    this->_data = data;
+Data::Data() : _dia(0), _mes(0), _ano(0){}
+
+Data::Data(int dia, int mes, int ano) : _dia(dia), _mes(mes), _ano(ano){}
+
+Data::Data(const std::string& data){
+    //Garantir que o formator seja: DD/MM/AAAA
+    assert(data.size()==10 && data[2] == '/' && data[5] == '/');
+    _dia = std::stoi(data.substr(0, 2));
+    _mes = std::stoi(data.substr(3, 2));
+    _ano = std::stoi(data.substr(6, 4));  
 }
 
-std::string Data::converte(){
-    std::string ds = std::to_string(_data);
-    return ds;
+int Data::get_dia() const{
+    return _dia;
+}
+    
+int Data::get_mes() const{
+    return _mes;
+}
+    
+int Data::get_ano() const{
+    return _ano;
+}
+    
+int Data::to_int() const{
+    return _ano*10000 + _mes*100 + _dia;
 }
 
-bool Data::compara(const int& a, const int& b){
-    if(a > b)
-        return true;
-    return false;
+bool Data::operator<(const Data& outro) const{
+    return to_int() < outro.to_int();
+} 
+
+bool Data::operator<=(const Data& outro) const{
+    return to_int() <= outro.to_int();
+}
+
+bool Data::operator>(const Data& outro) const{
+    return to_int() > outro.to_int();
+}
+
+bool Data::operator==(const Data& outro) const{
+    return to_int() == outro.to_int();
+}
+
+void Data::imprime() const{
+    std::cout << std::setfill('0') << std::setw(2) << _dia << "/" << std::setw(2) << _mes << "/" << std::setw(4) << _ano;
 }
--- data.h
+++ data.h
@@ -4,10 +4,22 @@
 
 class Data{
 private:
-    int _data;
+    int _dia;
+    int _mes;
+    int _ano;
 public:
-    Data(int data);
-    std::string converte();
-    bool compara(const int& a,const int& b);
+    Data();
+    Data(int dia, int mes, int ano);
+    Data(const std::string& data); //Construtor para receber data como string
+    int get_dia() const;
+    int get_mes() const;
+    int get_ano() const;
+    int to_int() const; //converte a data. Ex: 11/12/2024 fica 20241211
+    //Como a ordem importa, converter dessa maneira fica mais fácil de ordenar para a busca binária
+    bool operator<(const Data& outro) const; //Operators para a comparação das datas
+    bool operator<=(const Data& outro) const;
+    bool operator>(const Data& outro) const;
+    bool operator==(const Data& outro) const;
+    void imprime() const;
 };
 #endif
