7f6a2e0fb97a446db5af7e476282e1d4ea4645c6
Kayke <kaykerodr5@gmail.com>
2026-05-12 00:04:48 -0300

    Final

--- carteira.cpp
+++ carteira.cpp
@@ -132,9 +132,9 @@ void Carteira::ordenar(const std::string& criterio) {
             return a.quantidade > b.quantidade;
         });
     } else if (criterio == "preco") {
-        _acoes.mergeSort([](const Acao& a, const Acao& b) {
-            return (long long)a.custoTotal * b.quantidade > (long long)b.custoTotal * a.quantidade;
-        });
+    _acoes.mergeSort([](const Acao& a, const Acao& b) {
+        return a.custoTotal > b.custoTotal;
+    });
     } else if (criterio == "dividendo") {
         _acoes.mergeSort([](const Acao& a, const Acao& b) {
             return a.dividendoAcumulado < b.dividendoAcumulado;
